48d434984fa9f014e0531800da64a27296753ee1
carlos padial <surreal@asturnazari.com>
2016-02-24 21:55:46 +0100

    lifePromise.js - HaiKode

---
+++ lifePromise.js
@@ -0,0 +1,10 @@
+// lifepromise.js - HaiKode
+
+whenSad().then(relax =>
+    imagine(sun) && relax()
+  ).catch(problems =>
+    imagine(sun && burn(problems))
+  );
+
+// CC-Zer0-2016: 
+// @surreal6 @roboe @soyguijarro
